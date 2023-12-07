03d53136dfcae67afa64ae347b262bf41f76b6dd
seungryeol <seungryeol156@gmail.com>
2023-12-07 20:04:36 +0900

    fmt 테스트

--- mail/mail.go
+++ mail/mail.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 	"github.com/joho/godotenv"
 	"gopkg.in/gomail.v2"
-	"io/ioutil"
+	"io"
 	"log"
 	"net/http"
 	"os"
@@ -48,6 +48,8 @@ func Send() {
 	}
 
 	// 각 watcher에게 이메일 보내기
+	fmt.Println("Sending emails to watchers")
+	fmt.Println(watchers)
 	for _, watcher := range watchers {
 		err := sendEmail(watcher)
 		if err != nil {
@@ -59,7 +61,10 @@ func Send() {
 }
 
 func getWatchersList(owner, repo string) ([]string, error) {
+
+	// https://docs.github.com/en/rest/activity/watching?apiVersion=2022-11-28#list-watchers 이걸로
 	url := fmt.Sprintf("%s/repos/%s/%s/subscribers", githubAPIURL, owner, repo)
+	//fmt.Println(url)
 
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
@@ -69,30 +74,42 @@ func getWatchersList(owner, repo string) ([]string, error) {
 	req.Header.Add("Authorization", "Bearer "+githubToken)
 
 	resp, err := http.DefaultClient.Do(req)
+	//fmt.Println("resp")
+	//fmt.Println(resp)
 	if err != nil {
 		return nil, err
 	}
 	defer resp.Body.Close()
 
-	body, err := ioutil.ReadAll(resp.Body)
+	body, err := io.ReadAll(resp.Body)
+	//fmt.Println("body")
+	//fmt.Println(body)
+	//fmt.Println("body string")
+	//fmt.Println(string(body))
 	if err != nil {
 		return nil, err
 	}
 
+	// 여기서 에러 발생
 	var watchers []map[string]interface{}
 	err = json.Unmarshal(body, &watchers)
 	if err != nil {
 		return nil, err
 	}
+	//fmt.Println("watchers")
+	//fmt.Println(watchers)
 
 	var watcherList []string
 	for _, watcher := range watchers {
+		//fmt.Println(watcher)
+		//fmt.Println(watcher["login"])
 		watcherList = append(watcherList, watcher["login"].(string))
 	}
 
 	return watcherList, nil
 }
 
+// DSeung과 같은 형태로 옴
 func sendEmail(to string) error {
 	m := gomail.NewMessage()
 	m.SetHeader("From", senderEmail)                   // 송신자 이메일 주소
